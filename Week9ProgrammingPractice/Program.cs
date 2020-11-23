72215311b020df7792674aa37c5618c99a3d62de
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2020-11-23 10:37:54 +0000

    Add project files.

---
+++ Week9ProgrammingPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9ProgrammingPractice
+{
+    /*##########################################################################################################################
+     * 
+     * Week9ProgrammingPractice
+     * Started : 23/11/2020
+     * Last Edited : 23/11/2020
+     * Jake Beecham
+     * S00197638
+     * 
+    ###########################################################################################################################*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }//End of Main Method
+
+    }//End of Program Class
+}
