600c5392a8ba7b52fb7aa81cd09933dba46b5488
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2020-11-23 11:13:03 +0000

    Added Songs & Created Playlist.

--- Week9ProgrammingPractice/Program.cs
+++ Week9ProgrammingPractice/Program.cs
@@ -20,9 +20,34 @@ namespace Week9ProgrammingPractice
     {
         static void Main(string[] args)
         {
+            Song song1 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Pop);
+            Song song2 = new Song("RIDE IT", "REGARD", 3.37, Genre.Dance);
+            Song song3 = new Song("DANCE MONKEY", "TONES & I", 4.20, Genre.Dance);
+            Song song4 = new Song("CIRCLES", "POST MALONE", 3.25, Genre.Pop);
+            Song song5 = new Song("SOUTH OF THE BORDER", "ED SHEERAN", 4.26, Genre.Other);
 
+            List<Song> playlist = new List<Song>();
+
+            playlist.Add(song1);
+            playlist.Add(song2);
+            playlist.Add(song3);
+            playlist.Add(song4);
+            playlist.Add(song5);
+
+            DisplayPlaylist(playlist);
+
+            Console.ReadLine();
 
         }//End of Main Method
+
+        static void DisplayPlaylist(List<Song> playlist)
+        {
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine(song);
+            }
+
+        }//End of DisplayPlaylist Method
 
     }//End of Program Class
 }
